d97bf3d95efb9ba2f066182e4d15a7009611a6ff
Mouton.Navarro <Denmaniraven@yahoo.co.jp>
2022-09-30 16:34:15 +0900

    Create daily report show action

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -137,4 +137,25 @@ public class ReportAction extends ActionBase {
 		}
 
 	}
+
+	/**
+	 * Displays detail screen
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	public void show() throws ServletException, IOException{
+
+		//Acquires the report data with ID as condition
+		ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+
+		if (rv == null) {
+			//Displays error screen if corresponds report data is not exists
+			forward(ForwardConst.FW_ERR_UNKNOWN);
+		}else {
+			putRequestScope(AttributeConst.REPORT, rv);
+
+			//Displays detail screen
+			forward(ForwardConst.FW_REP_SHOW);
+		}
+	}
 }
