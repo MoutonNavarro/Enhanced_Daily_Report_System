4de16458d9ecff173f1b609967f3ad32dd8df018
Mouton.Navarro <Denmaniraven@yahoo.co.jp>
2022-10-03 22:18:37 +0900

    Localize trying 221003

--- src/main/java/constants/jp/ForwardConst.java
+++ src/main/java/constants/jp/ForwardConst.java
@@ -1,5 +1,66 @@
 package constants.jp;
 
+/**
+ * Enum class that defines relative the forward screen that request parameter variable names, variable value, jsp file's name, and else.
+ */
 public enum ForwardConst {
+	//action
+	ACT("action"),
+	ACT_TOP("Top"),
+	ACT_EMP("Employee"),
+	ACT_REP("Report"),
+	ACT_AUTH("Auth"),
 
+	//command
+	CMD("command"),
+	CMD_NONE(""),
+	CMD_INDEX("index"),
+	CMD_SHOW("show"),
+	CMD_SHOW_LOGIN("showLogin"),
+	CMD_LOGIN("login"),
+	CMD_LOGOUT("logout"),
+	CMD_NEW("entryNew"),
+	CMD_CREATE("create"),
+	CMD_EDIT("edit"),
+	CMD_UPDATE("update"),
+	CMD_DESTROY("destroy"),
+
+	//jsp
+	FW_ERR_UNKNOWN("jp/error/unknown"),
+	FW_TOP_INDEX("jp/topPage/index"),
+	FW_LOGIN("jp/login/login"),
+	FW_EMP_INDEX("jp/employees/index"),
+	FW_EMP_SHOW("jp/employees/show"),
+	FW_EMP_NEW("jp/employees/new"),
+	FW_EMP_EDIT("jp/employees/edit"),
+	FW_REP_INDEX("jp/reports/index"),
+	FW_REP_SHOW("jp/reports/show"),
+	FW_REP_NEW("jp/reports/new"),
+	FW_REP_EDIT("jp/reports/edit"),
+
+	//for configure
+	//action
+	ACT_CONFIG("Config"),
+
+	//jsp
+	FW_CONFIG("jp/configure/config");
+
+	/**
+	 * Character strings
+	 */
+	private final String text;
+
+	/**
+	 * Constructor
+	 */
+	private ForwardConst(final String text) {
+		this.text = text;
+	}
+
+	/**
+	 * Acquire the value(String)
+	 */
+	public String getValue() {
+		return this.text;
+	}
 }
--- src/main/java/constants/jp/HtmlConst.java
+++ src/main/java/constants/jp/HtmlConst.java
@@ -2,4 +2,59 @@ package constants.jp;
 
 public enum HtmlConst {
 
+	//General
+	LANGUAGE("日本語(JP)"),
+
+	//Base
+	TEXT_NAME("氏名"),
+	TEXT_DATE("日付"),
+	TEXT_TITLE("タイトル"),
+	TEXT_ACTION("操作"),
+	TEXT_SEEDETAIL("詳細を見る"),
+	TEXT_NUMBER_OF_ALL_ITEMS("(全: ${count} 件)"),
+
+	//Report
+	TEXT_REP_NUMBER_OF_ALL_ITEMS("(全: ${reports_count} 件)"),
+	TEXT_REP_SUMBIT_NEW("日報を新しく登録する"),
+	TEXT_REP_MINE("[あなたの日報リスト]"),
+
+	//jsps
+	//app.jsp
+	//Header
+	TEXT_HEAD_DAILY("日報管理システム"),
+	TEXT_HEAD_DAILY_TITLE("日報管理システム"),
+	TEXT_DISCRIPT_MAIN("ムートン・ナヴァーロの拡張日報管理システムです"),
+	TEXT_HEAD_MNGEMP("従業員管理"),
+	TEXT_HEAD_MNGREP("日報管理"),
+	TEXT_HEAD_USERNAME("	こんにちは!&nbsp;<c:out value=\"${sessionScope.login_employee.name}\" /> さん&nbsp;&nbsp;&nbsp;"),
+
+	//Footer
+	TEXT_DISPLAY_LANG("日本語"),
+
+	//topPage/index.jsp
+	TEXT_WELCOME("日報管理システムにようこそ!"),
+
+	//error/unknown.jsp
+	TEXT_UNKNOWN("お探しのページは見つかりませんでした"),
+	TEXT_UNKNOWN_FOLLOW("<c:out value='${followPage}' /> は見つかりませんでした");
+
+	/**
+	 * Field
+	 */
+	private final String htmlText;
+
+	/**
+	 * Constructor
+	 */
+	private HtmlConst(final String htmlText) {
+		this.htmlText = htmlText;
+	}
+
+	/**
+	 * Acquire the value(String)
+	 */
+	public String getValue() {
+		return this.htmlText;
+	}
+
 }
